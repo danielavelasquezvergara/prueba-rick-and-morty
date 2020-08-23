0b31f8eaa76cedf5a4591fabd17b42008344748d
Daniela <dani.velasquezv@gmail.com>
2020-08-23 01:03:51 -0400

    Cambios mejoras

--- src/index.js
+++ src/index.js
@@ -4,22 +4,18 @@ import $ from 'jquery';
 rickMorty.then((personajes) => {
     const person  = document.getElementById('personajes')
     personajes.forEach(element => {
-        person.innerHTML +=  `        
-    <div class="card mb-3" style="max-width: 540px;">
-    <div class="row no-gutters">
-      <div class="col-md-4">
-        <img id="${element.id}" src="${element.img}" class="card-img imagen rounded-circle" alt="image">
-      </div>
-      <div class="col-md-8">
-        <div class="card-body ${element.id}" ">
-          <h5 class="card-title">${element.nombre}</h5>
-          <p class="card-text">${element.genero}</p>
-          <p class="card-text"><small class="text-muted">${element.especies}</small></p>
-          <p class="card-text"><small class="text-muted">${element.estado}</small></p>
-        </div>
-      </div>
+        person.innerHTML +=  `
+        
+        
+  <div class="card m-3">
+    <img id="${element.id}" src="${element.img}" class="card-img-top imagen rounded-circle" alt="image">
+    <div class="card-body ${element.id}">
+      <h5 class="card-title">${element.nombre}</h5>
+      <p class="card-text">${element.genero}</p>
+      <p class="card-text"><small class="text-muted">${element.especies}</small></p>
+      <p class="card-text"><small class="text-muted">${element.estado}</small></p>
     </div>
-  </div>  `
+  </div>`
         
     });
     
